ebfd2d5b16113c34969151cde6c2e6c3d106247d
Christian Vogt <hi@chris.ac>
2015-09-17 15:21:05 +0200

    Test prep story4.4

---
+++ tests/e2e/capevent-list_spec.js
@@ -0,0 +1,17 @@
+describe('Event list', function() {
+
+    var baseURL = 'http://localhost:8000/#/capevent/list';
+    
+    beforeEach(function() {
+        browser.get(baseURL);
+    });
+  
+    it('should be not empty', function () {
+        
+    });
+        
+    it('should show correct data of Paddeltour Cap Event in the list', function() {
+        
+    });
+    
+});
