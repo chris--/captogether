a3df5c24c9903998b9b19fee12a11131c87401cd
Christian Vogt <hi@chris.ac>
2015-09-17 16:03:42 +0200

    Story 5.1 done

--- public/js/CapEvent/CapEventListController.js
+++ public/js/CapEvent/CapEventListController.js
@@ -1,5 +1,16 @@
 var capEventModule = angular.module('capEventModule');
 
 capEventModule.controller('CapEventListController', function ($scope, $location, CapEventService) {
-    $scope.capEvents = CapEventService.query(); 
+    $scope.capEvents = CapEventService.query();
+    $scope.showDetail = function (id) {
+        $location.path('/capevent/detail/' + id);
+    };
+    $scope.addParticipant = function (id) {
+        CapEventService.get({id: id}, function(capEvent) {
+            capEvent.participants++;
+            capEvent.$update({id: id}, function() {
+                $scope.capEvents = CapEventService.query();
+            });
+        });
+    }; 
 });
