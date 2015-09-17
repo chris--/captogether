061c26d326c220b8885fcf563f481dcf170f028c
Christian Vogt <hi@chris.ac>
2015-09-17 11:06:51 +0200

    story3.1 done

--- public/js/CapEvent/CapEventDetailController.js
+++ public/js/CapEvent/CapEventDetailController.js
@@ -1,15 +1,27 @@
 var capEventModule = angular.module('capEventModule');
 
-capEventModule.controller('CapEventDetailController', function ($scope, $routeParams, $location) {
-    $scope.capEvent = {
-    	id: 1, 
-		title: 'Ein CapEvent',
-		date: '01.01.2001',
-		category: 'Literatur',
-		participants: 1,
-		geolocation: '10.0001, 10.0001'
-    };
-    $scope.saveCapEvent = function() {
-    	console.log('Saving', $scope.capEvent);
+capEventModule.controller('CapEventDetailController', function ($scope, $routeParams, $location, CapEventService) {
+	if ($routeParams.id) {
+        // retrieve event from routeParam.capEventId and store in scope
+        $scope.capEvent = CapEventService.get({id: $routeParams.id});
+        $scope.saveCapEvent = function () {
+            $scope.capEvent.$update({id: $routeParams.id}, function () {
+            	console.log('Successfully updated', $scope.capEvent);
+                $location.path('/capevent/list/');
+            }, function(err) {
+                console.log('Error while updating', $scope.capEvent, ': ', err);
+            });
+        };
+    } else {
+        // no capEventId, create an empty CapEvent
+        $scope.capEvent = {};
+        $scope.saveCapEvent = function () {
+            console.log('Creating', $scope.capEvent);
+            $scope.capEvent = CapEventService.save($scope.capEvent, function () {
+                $location.path('/capevent/list/');
+            }, function(err) {
+                console.log('Error while saving', $scope.capEvent, ': ', err);
+            });
+        };
     }
 });
--- public/js/CapEvent/CapEventService.js
+++ public/js/CapEvent/CapEventService.js
@@ -4,5 +4,9 @@ var capEventModule = angular.module('capEventModule', ['ngResource']);
 var endpoint = 'http://localhost:3000/api';
 
 capEventModule.factory('CapEventService', function ($resource) {
-	// CapEventService
+    return $resource(endpoint + '/capevents/:id', null, {
+        update: {
+            method: 'PUT'
+        }
+    });
 });
