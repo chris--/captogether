c545b348c2bedd08f6b6f07f7c5ab3c44c2dcaea
Christian Vogt <hi@chris.ac>
2015-09-17 15:21:21 +0200

    story4.4 done

--- tests/e2e/capevent-list_spec.js
+++ tests/e2e/capevent-list_spec.js
@@ -8,10 +8,23 @@ describe('Event list', function() {
   
     it('should be not empty', function () {
         
+        var capEvents = element.all(by.repeater('capEvent in capEvents'));
+        expect(capEvents.count()).toBeGreaterThan(0);
+        
     });
         
     it('should show correct data of Paddeltour Cap Event in the list', function() {
         
+        var currentTitle = element(by.repeater('capEvent in capEvents').row(0).column('title')).getText();
+        var currentDate = element(by.repeater('capEvent in capEvents').row(0).column('date')).getText();
+        var currentCategory = element(by.repeater('capEvent in capEvents').row(0).column('category')).getText();
+        var currentParticipants = element(by.repeater('capEvent in capEvents').row(0).column('participants')).getText();              
+        
+        expect(currentTitle).toEqual('Paddeltour');
+        expect(currentDate).toEqual('19 Sep 2015');
+        expect(currentCategory).toEqual('Sport');
+        expect(currentParticipants).toEqual('10');        
+       
     });
     
 });
