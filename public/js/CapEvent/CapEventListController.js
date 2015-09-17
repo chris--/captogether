fcfd4cd5bef773bc0d6043aebfebf5994f41b016
Christian Vogt <hi@chris.ac>
2015-09-17 11:15:46 +0200

    Story3.2 done

--- public/js/CapEvent/CapEventListController.js
+++ public/js/CapEvent/CapEventListController.js
@@ -1,19 +1,5 @@
 var capEventModule = angular.module('capEventModule');
 
-capEventModule.controller('CapEventListController', function ($scope, $location) {
-    // CapEventListController
-    $scope.helloWorld = 'Hallo, List-View!'
-    $scope.capEvents = [{
-    	id: 1, 
-		title: 'Ein CapEvent',
-		date: '01.01.2001',
-		category: 'Literatur',
-		participants: 1
-    }, {
-    	id: 2,
-    	title: 'Noch ein CapEvent',
-    	date: '01.02.2010',
-		category: 'Sport',
-		participants: 2
-    }];
+capEventModule.controller('CapEventListController', function ($scope, $location, CapEventService) {
+    $scope.capEvents = CapEventService.query(); 
 });
