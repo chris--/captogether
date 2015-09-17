596df323b6cf95d55fd68b6b39afbba314576fcc
Christian Vogt <hi@chris.ac>
2015-09-17 10:59:25 +0200

    story2.5 done

--- public/js/CapEvent/CapEventDetailController.js
+++ public/js/CapEvent/CapEventDetailController.js
@@ -1,6 +1,15 @@
 var capEventModule = angular.module('capEventModule');
 
 capEventModule.controller('CapEventDetailController', function ($scope, $routeParams, $location) {
-    // CapEventDetailController
-    $scope.helloWorld = 'Hallo, Detail-View!'
+    $scope.capEvent = {
+    	id: 1, 
+		title: 'Ein CapEvent',
+		date: '01.01.2001',
+		category: 'Literatur',
+		participants: 1,
+		geolocation: '10.0001, 10.0001'
+    };
+    $scope.saveCapEvent = function() {
+    	console.log('Saving', $scope.capEvent);
+    }
 });
